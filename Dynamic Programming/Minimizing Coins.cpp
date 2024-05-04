e97f8591fe898a6bb228d24ecdb97248a0097477
Prakash Mondal <97402824+rko0211@users.noreply.github.com>
2024-05-05 01:51:26 +0530

    Create Minimizing Coins.cpp

---
+++ Dynamic Programming/Minimizing Coins.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+void solve(){
+    int n,x; cin>>n>>x;
+    int c[n];  for(int i=0;i<n;i++) cin>>c[i];
+    vector<int>dp(x+1,INT_MAX);
+    dp[0]=0;  // Number of coins required to reachval =x
+    for(int i=1;i<=x;i++){
+        // For all value of sum
+        for(int j=0;j<n;j++){
+            if(c[j]<=i && dp[i-c[j]]!=INT_MAX){
+                dp[i] = min(dp[i],dp[i-c[j]]+1);
+            }
+        }
+    }
+ if(dp[x] == INT_MAX){
+    cout<<-1<<endl;
+  }
+  else{
+    cout<<dp[x]<<endl;
+  }
+ 
+}
+int main(){
+ 
+solve();
+ 
+    return 0;
+}
